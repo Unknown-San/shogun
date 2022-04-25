444d2d32f405a17d8df20de913f128bfbc97e660
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-04-25 08:47:17 +0530

    Update __main__.py

--- ShuKurenaiXRoBot/__main__.py
+++ ShuKurenaiXRoBot/__main__.py
@@ -93,7 +93,7 @@ PM_START_TEXT = """
 buttons = [
     [
         InlineKeyboardButton(
-            text="➗ Add Shu Kurenai To Your Group ➗", url="t.me/ShuKurenaiXRoBot?startgroup=new"),
+            text="➗ Add Shu Kurenai To Your Group ➗", url="t.me/RaidenSuperBot?startgroup=new"),
     ],
     [
         InlineKeyboardButton(
